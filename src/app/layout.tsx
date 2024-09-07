0c1840149e1c434070cfe7fe45e7c33a73ab86f5
Pablo Santos <pablorasantos.dev@gmail.com>
2024-09-06 22:54:10 -0300

    feat: criando layout da aplicacao

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="pt" className={inter.variable}>
       <body>{children}</body>
     </html>
   )
