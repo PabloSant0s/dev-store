8c758a2293d149be9b1c9179d8a8a7f60f6babba
Pablo Santos <pablorasantos.dev@gmail.com>
2024-09-08 15:10:20 -0300

    feat: fetch produtos na Home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,15 +1,24 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+
+async function getFeaturedProducts(): Promise<Product[]> {
+  const response = await api('/products/featured')
+  return await response.json()
+}
+
 export default async function Home() {
+  const [highlightedProduct, ...othersProducts] = await getFeaturedProducts()
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
       >
         <Image
-          src="/moletom-never-stop-learning.png"
+          src={highlightedProduct.image}
           width={920}
           height={920}
           quality={100}
@@ -17,51 +26,47 @@ export default async function Home() {
           className="transition-transform duration-500 group-hover:scale-105"
         />
         <div className="absolute bottom-28 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="truncate text-sm">Moleton Never Stop Learning</span>
+          <span className="truncate text-sm">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
+            {highlightedProduct.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0,
+            })}
           </span>
         </div>
       </Link>
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
-      >
-        <Image
-          src="/moletom-java.png"
-          width={920}
-          height={920}
-          quality={100}
-          alt=""
-          className="transition-transform duration-500 group-hover:scale-105"
-        />
+      {othersProducts.map((product) => {
+        return (
+          <Link
+            key={product.id}
+            href={`/product/${product.slug}`}
+            className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+          >
+            <Image
+              src={product.image}
+              width={920}
+              height={920}
+              quality={100}
+              alt=""
+              className="transition-transform duration-500 group-hover:scale-105"
+            />
 
-        <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="truncate text-sm">Moleton Java</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$129
-          </span>
-        </div>
-      </Link>
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
-      >
-        <Image
-          src="/camiseta-dowhile-2022.png"
-          width={920}
-          height={920}
-          quality={100}
-          alt=""
-          className="transition-transform duration-500 group-hover:scale-105"
-        />
-        <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-          <span className="truncate text-sm">Camiseta Do While 2022 </span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$89
-          </span>
-        </div>
-      </Link>
+            <div className="absolute bottom-10 right-28 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="truncate text-sm">{product.title}</span>
+              <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                {product.price.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}
+              </span>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   )
 }
--- src/data/api.ts
+++ src/data/api.ts
@@ -2,7 +2,9 @@ import { env } from '@/env'
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env!.NEXT_PUBLIC_API_BASE_URL
-  const url = new URL(baseUrl, path)
+  const apiKey = '/api'
+  const url = new URL(apiKey.concat(path), baseUrl)
+  console.log(url)
 
   return fetch(url, init)
 }
