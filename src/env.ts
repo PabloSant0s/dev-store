3aef951b4ac3d1c67e7c5d256fdd5a4118b52aa0
Pablo Santos <pablorasantos.dev@gmail.com>
2024-09-08 13:51:38 -0300

    feat: criando fetch api wrapper

---
+++ src/env.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod'
+
+export const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+})
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+  console.error(
+    'Invalid environment variables',
+    parsedEnv.error.flatten().fieldErrors,
+  )
+}
+
+export const env = parsedEnv.data
