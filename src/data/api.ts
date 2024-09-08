fe7d5a52e0d0971b229a59deff0ab61575920b84
Pablo Santos <pablorasantos.dev@gmail.com>
2024-09-08 15:11:30 -0300

    feat: chache e memoization

--- src/data/api.ts
+++ src/data/api.ts
@@ -4,7 +4,6 @@ export function api(path: string, init?: RequestInit) {
   const baseUrl = env!.NEXT_PUBLIC_API_BASE_URL
   const apiKey = '/api'
   const url = new URL(apiKey.concat(path), baseUrl)
-  console.log(url)
 
   return fetch(url, init)
 }
