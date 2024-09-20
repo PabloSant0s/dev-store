5e6e79447db435392b5ec294500aa42ea1368be7
Pablo Santos <pablorasantos.dev@gmail.com>
2024-09-19 22:27:32 -0300

    feat: formulario de busca

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,36 @@
 import { Search } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
 import { CartWidget } from './cart-widget'
 
 export default function Header() {
+  async function handleSearch(data: FormData) {
+    'use server'
+    const query = data.get('q')
+
+    if (!query) return
+
+    redirect(`/search?q=${query}`)
+  }
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-5">
         <Link href="/" className="text-2xl font-extrabold text-white">
           devstore
         </Link>
-        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+        <form
+          action={handleSearch}
+          method="GET"
+          className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
+        >
           <Search className="h-5 w-5 text-zinc-500" />
           <input
             placeholder="Buscar produtos..."
             className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+            name="q"
+            required
           />
         </form>
       </div>
